3fb3ff5a5cf9a060ce93520de7c8d63899ade139
AhmadRikiYakub <rikiyakub2105@gmail.com>
2024-11-22 10:39:20 +0700

    Index.php

---
+++ Index.php
@@ -0,0 +1,19 @@
+<?php
+$nama   = "Ahmad Riki Yakub";
+$Kelas  = "TI.23.CA.1";
+$Nim    = "312310014";
+$Prodi  = "Tekhnik Informatika";
+?>
+<h1>
+Saya Adalah 
+<?php echo $nama?>
+</h1>
+<p>
+ <strong>Kelas</strong>=<?php echo $Kelas?>
+</p>
+<p>
+ <strong>Nim</strong>=<?php echo $Nim?>
+</p>
+<p>
+<strong>Prodi</strong>=<?php echo $Prodi?>
+</p>
